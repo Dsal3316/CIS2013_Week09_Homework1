49c56e3e1b8138829009b69b5005f17dded2cd69
Dsal3316 <35541000+Dsal3316@users.noreply.github.com>
2018-03-24 17:25:28 -0500

    first commit file format

---
+++ CIS2013_Week09_Homework1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+	char encrypt;
+	char decrypt;
+
+int main (){
+	
+	int file;
+	
+	cout << "Please enter name of file to encrypt/decrypt: " << endl;
+	cin >> file;
+	cout << "Would you like to encrypt or decrypt a message: " << endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
