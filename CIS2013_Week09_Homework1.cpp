6185a26c42d8342ffa2cc95a0ed6cca45e1bf693
Dsal3316 <35541000+Dsal3316@users.noreply.github.com>
2018-03-28 04:41:10 -0500

    saving work not compiled

--- CIS2013_Week09_Homework1.cpp
+++ CIS2013_Week09_Homework1.cpp
@@ -4,23 +4,46 @@
 
 using namespace std;
 
-	char encrypt;
-	char decrypt;
+	char encrypt(char input, char keychar);
+	char decrypt(char input, char keychar);
 
 int main (){
 	
 	int file;
 	ifstream dat;
+	string f_name="file1.dat";
+	char action;
 	
 	
-	cout << "Please enter number of file to encrypt/decrypt: (ex: 1,2,3)" << endl;
-	cin >> file;
+	cout << "Please enter number of file to encrypt/decrypt: (file1.dat)" << endl;
+	getline (cin, f_name);
 	
-	if (file == '1' ){
+	if (f_name == "file1.dat" ){
 	dat.open("file1.dat");
+	}else
+	{
+		cout << "Could not find the file";
+		exit(1);
 	}
 	
-	cout << "Would you like to encrypt or decrypt a message: " << endl;
+	do {
+		cout << endl;
+		cout << "Would you like to encrypt(e) or decrypt(d) a message: " << endl;
+		cout endl;
+		cin >> action;
+		cin.ignore(50,'\n');
+		cout << endl;
+		
+		if (action == 'e') {
+				encr ();
+		}
+		else if (action == 'd'){
+			decr();
+		}
+		else {
+			cout << "Wrong action selected please try again" << endl;
+		}
+	}while (1);
 	
 	
 	
@@ -30,4 +53,21 @@ int main (){
 	
 	
 	return 0;
+}
+
+char encrypt(char input, char keychar);
+{
+	char x, y;
+	
+	if (input== ' ' ) x=27;
+	else x = input - 'a' + 1;
+	
+	if (keychar == ' ') x = 0;
+	else y = keychar - 'a' + 1;
+	
+	char R = x -y;
+	if (R < 0)
+		R += 27;
+	if (!R) return ' ';
+	return R + 'a'a-1;
 }
